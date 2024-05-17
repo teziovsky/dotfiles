0d23833043b90c26228da80667e5a6270722207d
Jakub Soboczyński <teziovsky@gmail.com>
2024-05-17 09:49:43 +0200

    2024-05-17 09:48:49: update dotfiles

--- .install_macos/1_packages.sh
+++ .install_macos/1_packages.sh
@@ -333,7 +333,7 @@ echo -e "\n"
 echo "Installing Homebrew Developer Casks..."
 echo "------------------------------------------------"
 
-casksDeveloper=(warp slack figma linear-linear height orbstack bruno tableplus visual-studio-code zed)
+casksDeveloper=(warp slack figma linear-linear height orbstack tableplus visual-studio-code zed)
 for developerCask in "${casksDeveloper[@]}"; do
   if ! ls $(brew --caskroom) | grep -w "^$developerCask$" &>/dev/null; then
     brew install --cask $developerCask -q
